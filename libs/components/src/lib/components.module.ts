b63a34acd143ee313fd13377aba21fa38283c184
Hrvoje Bernardić <hbernardic@omega-software.hr>
2020-01-21 18:40:50 +0100

    feat(components): add dummy component

--- libs/components/src/lib/components.module.ts
+++ libs/components/src/lib/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from '@floyd-devops/core';
+import { ComponentComponent } from './component/component.component';
 
 @NgModule({
-  imports: [CommonModule, CoreModule]
+  imports: [CommonModule, CoreModule],
+  declarations: [ComponentComponent]
 })
 export class ComponentsModule {}
