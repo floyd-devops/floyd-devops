9f1662fdf32a04de524470e571710fd2425923dc
Hrvoje Bernardić <hbernardic@omega-software.hr>
2020-01-21 19:10:30 +0100

    fix(components): add sidebar component
    sidebar to use with app layout

--- libs/components/src/lib/components.module.ts
+++ libs/components/src/lib/components.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from '@floyd-devops/core';
 import { ComponentComponent } from './component/component.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [CommonModule, CoreModule],
-  declarations: [ComponentComponent]
+  declarations: [ComponentComponent, SidebarComponent]
 })
 export class ComponentsModule {}
