50aa738dc5cc5956d162338223276f44fe5e5b00
Hrvoje Bernardić <hbernardic@omega-software.hr>
2020-01-21 13:17:47 +0100

    initial commit

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'floyd-devops-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'playground';
+}
