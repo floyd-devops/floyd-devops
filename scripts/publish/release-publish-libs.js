f3329a0ff3f08cbd3b9904ac8a1c1695a64bb6b4
Hrvoje Bernardić <hrvbernardic@gmail.com>
2020-01-23 23:04:02 +0100

    ci(github): add pr action

--- scripts/publish/release-publish-libs.js
+++ scripts/publish/release-publish-libs.js
@@ -1,18 +1,18 @@
 const {execSync} = require('child_process');
 const chalk = require('chalk');
-const {getRepoName} = require('../util');
 const fs = require('fs');
 
 const updateRepoVersion = () => {
-  const fileName = '../../package';
+  const fileName = '../../package.json';
   let file = require(fileName);
   file.version = execSync('git describe').toString().substr(1);
 
   fs.writeFile(fileName, JSON.stringify(file), function (err) {
     if (err) return console.log(err);
+    // execSync('git add package.json');
+    // execSync(`git commit -m "chore(release): update repository version -> ${file.version}"`);
+    // execSync('git push -u origin master');
   });
-  execSync(`git commit -a -m "chore(release): update repository version -> ${file.version}"`);
-  execSync('git push -u origin master');
 };
 
 const releaseAndPublish = (version, force, yes) => {
